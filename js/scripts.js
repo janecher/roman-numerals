b301150dc4e1423966a51aa71d7310c62cb4ec98
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-11 11:32:27 -0700

    add spec to the README.md
    Co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
 //business logic
 const reverseNumber = function(number){
-  let result=0;
+  let result=[];
   while(number > 0){
     let remainder = number%10;
-    result = result*10 + remainder;
+    result.push(remainder); //= result*10 + remainder;
     number = Math.floor(number/10);
   }
   return result;
 }
+/*let convertDigitArray = [];
+let answer = "";
+for (let index = array.length-1; index >=0; index--){
+  answer += array[index];
+}*/
 
 $(document).ready(function(){
   $("form").submit(function(event){
